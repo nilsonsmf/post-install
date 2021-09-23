820275efdd4d37cff0b1857aa0b2d1db2d321d5d
nilsonsmf <51447494+nilsonsmf@users.noreply.github.com>
2021-09-23 16:16:53 -0300

    Update ubuntu-wsl-postinstall.sh

--- ubuntu-wsl-postinstall.sh
+++ ubuntu-wsl-postinstall.sh
@@ -37,7 +37,7 @@ wsl_tools() {
 
   # install editor/coding tools
   echod "installing programming tools"
-  sudo apt-get -y install build-essential 
+  sudo apt-get -y install build-essential php7.4-cli
 
   # install git and git-lfs
   echod "installing git and related tools"
